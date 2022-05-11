dc2c666f27c0d4bf744e1f9554453a9665460bd0
221bye <omfg5451@gmail.com>
2022-05-11 14:35:56 +1000

    added flags

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-
 	"github.com/221bye/img_to_ascii/convert"
 )
 
 func main() {
+	var fFlag = flag.String("f", "test_images/shrek.jpg", "path to image you want to convert")
+	var srWFlag = flag.Int("srW", 4, "width of rectangle that one symbol represents")
+	var srHFlag = flag.Int("srH", 5, "height of rectangle that one symbol represents")
+
+	flag.Parse()
+
 	opts := convert.Options{
-		SrHeight: 5,
-		SrWidth:  4,
-		Filepath: "test_images/shrek.jpg",
+		SrHeight: *srHFlag,
+		SrWidth:  *srWFlag,
+		Filepath: *fFlag,
 	}
 	res := convert.ConvertImage(opts)
 	fmt.Println(res)
